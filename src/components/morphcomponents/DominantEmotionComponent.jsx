31a42b71cfb415bf38325968b22e854000ef0fa4
Ahmed Eid <ahmedeid2504@gmail.com>
2024-04-09 01:04:08 +0200

    fix DominantEmotionComponent

--- src/components/morphcomponents/DominantEmotionComponent.jsx
+++ src/components/morphcomponents/DominantEmotionComponent.jsx
@@ -6,23 +6,19 @@ const GenderComponent = (props) => {
   const [dominantEmotion, setDominantEmotion] = useState("");
   
   useEffect(() => {
-    if (!props.userData.userName || props.isTyping) {
-      // If userName is empty, user is typing, or data has been sent recently, return without saving data
-      console.error("Please enter your name or wait until data can be sent again");
-      return;
-    }
     bindEvents();
   }, []);
 
   function bindEvents(){
     window.addEventListener("CY_FACE_EMOTION_RESULT", (evt) => {
       setDominantEmotion(evt.detail.output.dominantEmotion || "") ;
-      props.setUserData({...props.userData, dominantEmotion: dominantEmotion})
+      props.setUserData({...props.userData, dominantEmotion: dominantEmotion});
     });
   }
   return (
     <div >
-
+    <p style={{fontSize:"20px"}}>DominantEmotion Component:</p>
+     <p>{dominantEmotion}</p>
     </div>
   );
 };
--- src/components/morphcomponents/GenderComponent.jsx
+++ src/components/morphcomponents/GenderComponent.jsx
@@ -6,11 +6,6 @@ const GenderComponent = (props) => {
   const [gender, setGender] = useState("");
   
   useEffect(() => {
-    if (!props.userData.userName || props.isTyping) {
-      // If userName is empty, user is typing, or data has been sent recently, return without saving data
-      console.error("Please enter your name or wait until data can be sent again");
-      return;
-    }
     bindEvents();
   }, []);
 
@@ -22,8 +17,8 @@ const GenderComponent = (props) => {
   }
   return (
     <>
-    <p style={{fontSize:"20px"}}>Gender Component:</p>
-    <div style={{minHeight: "200px"}}>
+    <p>Gender Component:</p>
+    <div>
     <p>{gender}</p>
     {(gender.toLocaleLowerCase() === 'male') && <img alt="" src="image/male.png"/>}
     {(gender.toLocaleLowerCase() === 'female') && <img alt="" src="images/female.png"/>}
