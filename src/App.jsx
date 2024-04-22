445f1a923492d39b40ad73c48fe1a10db3653406
Ahmed Eid <ahmedeid2504@gmail.com>
2024-04-22 07:06:18 +0200

    add a pdf route that displays pdfs

--- src/App.jsx
+++ src/App.jsx
@@ -187,6 +187,7 @@ function App() {
                     <Route path="/quiz" element={<iframe className='h-screen w-full' src="/quiz.html" title="Quiz" />} />
                     <Route path="/moodle" element={<iframe name="moodleFrame" src="http://4.157.125.46" width="80%" height="800"></iframe>} />
                     <Route path="/video" element={<iframe className='h-[50vh] w-full' src="https://www.youtube.com/embed/bMknfKXIFA8" title="React Course - Beginner&#39;s Tutorial for React JavaScript Library [2022]" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>} />
+                    <Route path="/pdf" element={<iframe className='h-[75vh] w-full' src='https://drive.google.com/file/d/1t_l0lVYwMKDwooqYNjEaNinW3Bz05Hx0/preview' frameBorder="0"></iframe>} />
                 </Routes>
                 <Footer />
         </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,11 +57,11 @@ const Navbar =() => {
                     <div>
                         <div className="bg-c_3 text-center items-center justify-center w-[400px] -skew-x-12 h-[96px] top-0 -z-50 absolute  right-5 flex"></div>
                         <ul className="flex justify-between items-center list-none p-2 text-center gap-5">
-                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/login">Login</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/home">Home</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/quiz">Quiz</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/moodle">Moodle</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/video">Video</Link></li>
+                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/home">Home</Link></li>
+                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/quiz">Quiz</Link></li>
+                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/moodle">Moodle</Link></li>
+                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/pdf">Pdf</Link></li>
+                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/video">Video</Link></li>
                             <div className="flex justify-center items-center text-center content-center gap-3"> 
                                 <button className="hover:text-c_4 transition-all ease-in duration-200 text-2xl flex items-center justify-center"  onClick={toggleUserMenu}>
                                     <img src="images/user.png" alt="user settings icon" />
@@ -80,27 +80,32 @@ const Navbar =() => {
                         </ul>
                     </div>
                 ) : (
-                    <ul className="flex justify-between items-center list-none p-2 text-center gap-5">
-                        <div className="flex justify-center items-center text-center content-center gap-2">
-                            <button className="hover:text-c_4 transition-all ease-in duration-200 text-2xl flex items-center justify-center"  onClick={toggleUserMenu}>
-                                ☰
-                            </button>
-                        </div>
-                        {showUserMenu && (
-                            <ul className={showUserMenu ? "absolute bg-c_3 flex flex-col justify-center gap-3 items-center bg-third p-10 px-16 top-0 right-0 translate-y-20 h-auto w-auto transition-all ease-in duration-200" : "translate-y-20 transition-all ease-in duration-200"} ref={ulRef}>
-                                    <>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/login">Login</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/home">Home</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/quiz">Quiz</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/moodle">Moodle</Link></li>
-                                        <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/video">Video</Link></li>
-                                    </> 
-                                    {/* <>
-                                        <li><button className="hover:text-c_4 transition-all ease-in duration-200" >Sign Out</button></li>
-                                    </> */}
-                            </ul>
-                        )}
-                    </ul>
+                    <div>
+                        <div className="bg-c_3 text-center items-center justify-center w-[100px] -skew-x-12 h-[104px] top-0 -z-50 absolute  right-5 flex"></div>
+                        <ul className="flex justify-between items-center list-none p-2 text-center gap-5">
+                            <div className="flex justify-center items-center text-center content-center gap-2">
+                                <button className="hover:text-c_4 transition-all ease-in duration-200 text-2xl flex items-center justify-center"  onClick={toggleUserMenu}>
+                                    ☰
+                                </button>
+                            </div>
+                            {showUserMenu && (
+                                <ul className={showUserMenu ? "absolute bg-c_3 flex flex-col justify-center gap-3 items-center bg-third p-10 px-16 top-0 right-0 translate-y-20 h-auto w-auto transition-all ease-in duration-200" : "translate-y-20 transition-all ease-in duration-200"} ref={ulRef}>
+                                        <>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/login">Login</Link></li>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/home">Home</Link></li>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/quiz">Quiz</Link></li>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/moodle">Moodle</Link></li>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/pdf">Pdf</Link></li>
+                                            <li><Link className="hover:text-c_4 transition-all ease-in duration-200" to="/video">Video</Link></li>
+                                        </> 
+                                        {/* <>
+                                            <li><button className="hover:text-c_4 transition-all ease-in duration-200" >Sign Out</button></li>
+                                        </> */}
+                                </ul>
+                            )}
+                        </ul>
+
+                    </div>
                     
                 )}
             </div>
