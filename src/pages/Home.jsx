d9f13a368f96c18de50038d3e0056ef0d883088e
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-19 06:59:37 +0300

    refactor: Update sessionStarted logic in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -167,35 +167,6 @@ const Home = () => {
             window.removeEventListener("unload", handleUnload);
         };
     }, []);
-    
-    let fTimeoutId = null; // Declare a variable to hold the timeout ID
-    
-    async function saveToFirebase() {
-    if ( userDataChanged && sessionStarted && !isSendingData) {
-        const dataRef = ref(database, "data/" + username + "/" + userData.SessionStartedAt + "/");
-        const newDataRef = push(dataRef);
-
-        set(newDataRef, userData)
-            .then(() => {
-                console.log("Data saved to Firebase");
-            })
-            .catch(() => {
-                console.error("Error saving:");
-            })
-            .finally(() => {
-                if (fTimeoutId) {
-                    clearTimeout(fTimeoutId); // Clear the timeout if it exists
-                }
-                fTimeoutId = setTimeout(() => {
-                }, 15000); // 10-second delay
-            });
-    }
-}
-
-// Use useEffect to trigger saveToFirebase when userData changes
-useEffect(() => {
-    {sessionStarted && saveToFirebase()}
-}, [sessionStarted, userData, userDataChanged]);
 
 
     // sending data to django api
