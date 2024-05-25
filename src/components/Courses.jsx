124d3eb277b1727fd846258e1175e7f2c250d8d4
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-25 03:47:45 +0300

    Update Courses component to center the semester title

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -40,7 +40,7 @@ const Courses = () => {
 
             {selectedSemester && (
                 <div className='bg-c_5 p-5 flex flex-col shadow-md'>
-                    <h1 className="text-2xl font-bold mb-4">Courses for {selectedSemester}</h1>
+                    <h1 className="text-2xl self-center font-bold mb-4">Courses for {selectedSemester}</h1>
                     <div className="flex flex-wrap overflow-scroll h-[30dvh] max-sm:h-[20dvh] justify-start">
                         {courses.filter(course => course.semester === selectedSemester).map((course) => (
                             <div key={course.id} className="p-4 border flex justify-center items-center rounded-lg hover:bg-black hover:bg-opacity-10  shadow-md m-2 cursor-pointer">
