13a9034c56a91bbdcb15462b7d3ad7397e6066a9
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-27 03:48:05 +0300

    feat: Add lazy loading for images in App component
    Improve page load performance by implementing lazy loading for images in the App component. This ensures that images are only loaded when they are visible on the screen, reducing the initial load time and improving the overall user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Sessions from './components/reports/Sessions';
 import TotalSessions from './components/reports/TotalSessions';
 import QuizReport from './components/reports/QuizReports';
 function App() {
+    const adminEmails = import.meta.env.VITE_ADMIN_EMAILS.split(',');
 
     return (
         <div className="h-auto flex flex-col justify-between">
@@ -25,6 +26,7 @@ function App() {
                     <Route path="/" element={<Home  />} />
                     <Route path="/login" element={<Login />} />
                     {/* <Route path="/signup" element={<Signup />} /> */}
+                    {adminEmails.includes(localStorage.userEmail) && 
                     <>
                         <Route path="dashboard" element={<Dashboard />} >
                             <Route path='reports' element={<Reports />}/>
@@ -41,6 +43,7 @@ function App() {
                             <Route path="quizreports/:courseName" element={<QuizReport />} />
                         </Route>
                     </>
+                    }
                 </Routes>
             </Router>
             <Analytics />
