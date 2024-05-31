ab4d2c8a29af0a7f8f2d936be10fbd14e25c5547
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-31 07:56:25 +0300

    calculating avg emotion before setting it

--- src/components/morphcomponents/DominantEmotionComponent.jsx
+++ src/components/morphcomponents/DominantEmotionComponent.jsx
@@ -1,43 +1,56 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
+const GenderComponent = (props) => {
+  const emotionData = useRef([]);
 
-const GenderComponent = (props) => {  
-  const [ dominantEmotion, setDominantEmotion ] = useState("Neutral");
   useEffect(() => {
     bindEvents();
   }, []);
 
-  function bindEvents(){
-    window.addEventListener("CY_FACE_EMOTION_RESULT", handleEmotionEvent );
+  function bindEvents() {
+    window.addEventListener("CY_FACE_EMOTION_RESULT", handleEmotionEvent);
   }
 
-  function handleEmotionEvent(event){
-    setDominantEmotion(event.detail.dominantEmotion);
+  function handleEmotionEvent(event) {
+    // Push the new emotion data to the array
+    emotionData.current.push(event.detail.dominantEmotion);
+
+    // Keep the array size limited to the last 100 emotions
+    if (emotionData.current.length > 100) {
+      emotionData.current.shift();
+    }
   }
 
   useEffect(() => {
-    const interval = setInterval(handleSetEmotion, 5000); // Fetches data every 5 seconds
+    const interval = setInterval(handleSetEmotion, 5000); // Process data every 5 seconds
 
     return () => clearInterval(interval); // This is important to clear the interval when the component unmounts
-}, []);
+  }, []);
 
   function handleSetEmotion() {
-      // set userData from props to save dominantEmotion 
-      props.setUserData(prevUserData => ({
-        ...prevUserData,
-        dominantEmotion: dominantEmotion || "Neutral"
-      }));
-      console.log(dominantEmotion);
-      props.setUserDataChanged(true);
+    if (emotionData.current.length === 0) return;
+
+    // Calculate the most frequent emotion
+    const emotionCounts = emotionData.current.reduce((acc, emotion) => {
+      acc[emotion] = (acc[emotion] || 0) + 1;
+      return acc;
+    }, {});
+
+    const mostFrequentEmotion = Object.keys(emotionCounts).reduce((a, b) =>
+      emotionCounts[a] > emotionCounts[b] ? a : b
+    );
+
+    // Update the user data with the most frequent emotion
+    props.setUserData((prevUserData) => ({
+      ...prevUserData,
+      dominantEmotion: mostFrequentEmotion || "Neutral",
+    }));
+
+    props.setUserDataChanged(true);
   }
 
-  
-
-  return (
-    <div >
-    </div>
-  );
+  return <div></div>;
 };
 
 export default GenderComponent;
