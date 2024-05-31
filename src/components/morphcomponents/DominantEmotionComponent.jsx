9e6c19de71d2c3006af8374151c0b4cccb9fe41c
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-31 07:38:51 +0300

    revert

--- src/components/morphcomponents/DominantEmotionComponent.jsx
+++ src/components/morphcomponents/DominantEmotionComponent.jsx
@@ -8,23 +8,16 @@ const GenderComponent = (props) => {
   }, []);
 
   function bindEvents(){
-    window.addEventListener("CY_FACE_EMOTION_RESULT", handleEmotionEvent);
-  }
-
-  function handleEmotionEvent(evt) {
-    setTimeout(() => {
+    window.addEventListener("CY_FACE_EMOTION_RESULT", (evt) => {
       // set userData from props to save dominantEmotion 
       props.setUserData(prevUserData => ({
         ...prevUserData,
         dominantEmotion: evt.detail.output.dominantEmotion || "Neutral"
       }));
-      console.log(props.userData.dominantEmotion);
-    }, 5000); // Delay of 5 seconds
+      console.log(evt.detail.output.dominantEmotion);
+      props.setUserDataChanged(true)
+    });
   }
-  useEffect (() => {
-    handleEmotionEvent();
-  }, []);
-
   return (
     <div >
     </div>
