c50857b36202c3647b6e85bebdeeef5001f2e65a
Ahmed Eid <ahmedeid2504@gmail.com>
2024-05-31 08:27:39 +0300

    refactor: Improve handling of dominant emotion in DominantEmotionComponent.jsx

--- src/components/morphcomponents/DominantEmotionComponent.jsx
+++ src/components/morphcomponents/DominantEmotionComponent.jsx
@@ -1,35 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 
-const GenderComponent = (props) => { 
+const GenderComponent = (props) => {  
   useEffect(() => {
     bindEvents();
   }, []);
 
   function bindEvents(){
-    window.addEventListener("CY_FACE_EMOTION_RESULT", handleSetEmotion );
-  }
-
-
-  useEffect(() => {
-    const interval = setInterval(handleSetEmotion, 5000); // Fetches data every 5 seconds
-
-    return () => clearInterval(interval); // This is important to clear the interval when the component unmounts
-}, []);
-
-  function handleSetEmotion(event) {
+    window.addEventListener("CY_FACE_EMOTION_RESULT", (evt) => {
       // set userData from props to save dominantEmotion 
       props.setUserData(prevUserData => ({
         ...prevUserData,
-        dominantEmotion: event.detail.dominantEmotion === ('' || 'Undefined' || 'undefined') ? 'Neutral' : event.detail.dominantEmotion
+        dominantEmotion: evt.detail.dominantEmotion === ('' || 'Undefined' || 'undefined') ? 'Neutral' : evt.detail.dominantEmotion
       }));
-      console.log(event.detail.dominantEmotion);
-      props.setUserDataChanged(true);
+      props.setUserDataChanged(true)
+    });
   }
-
-  
-
   return (
     <div >
     </div>
