2a9c9e2e5b82e1b221f0da059df556b1b5c88458
Ahmed Eid <ahmedeid2504@gmail.com>
2024-06-02 07:20:26 +0300

    fix dominant emotion trial

--- src/components/morphcomponents/DominantEmotionComponent.jsx
+++ src/components/morphcomponents/DominantEmotionComponent.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 const GenderComponent = (props) => {  
+  const [dominantEmotion, setDominantEmotion] = useState("");
+
   useEffect(() => {
     bindEvents();
   }, []);
 
   function bindEvents(){
     window.addEventListener("CY_FACE_EMOTION_RESULT", (evt) => {
+      setDominantEmotion(evt.detail.output.dominantEmotion || "") ;
       // set userData from props to save dominantEmotion 
+      
       props.setUserData((prevData) => {
         return {
           ...prevData,
-          dominantEmotion: evt.detail.dominantEmotion
+          dominantEmotion: dominantEmotion
         }
       });
     });
