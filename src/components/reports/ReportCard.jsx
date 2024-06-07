d7cb4cbbd690d1b49dc7323d04df0c22d2ceb19d
Ahmed Eid <ahmedeid2504@gmail.com>
2024-06-07 05:57:41 +0300

    fix loading and width in reportcard

--- src/components/reports/ReportCard.jsx
+++ src/components/reports/ReportCard.jsx
@@ -32,10 +32,6 @@ const ReportCard = () => {
         return <div className='text-[red]'>Error: {error}</div>;
     }
 
-    if (grades.length === 0) {
-        return <div className='dark:text-white'>Loading...</div>;
-    }
-
     if (isLoading) {
         return <div className='bg-c_3 dark:bg-dark-grey justify-center items-center rounded-lg flex p-5'>
             <img className="w-[20vw] animate-pulse" src="/images/logo.png" alt="" />
@@ -45,7 +41,7 @@ const ReportCard = () => {
     return (
         <div className="max-w-4xl mx-auto p-4 bg-c_5 dark:bg-black shadow-md rounded-lg">
             <h1 className="text-2xl font-bold dark:text-white mb-4">Report Card</h1>
-            <div className='overflow-auto dark:bg-black dark:bg-opacity-25 w-[70vw]'>
+            <div className='overflow-auto dark:bg-black dark:bg-opacity-25 '>
                 <table className="min-w-full bg-c_5 dark:bg-black dark:bg-opacity-25 dark:text-white">
                     <thead>
                         <tr>
